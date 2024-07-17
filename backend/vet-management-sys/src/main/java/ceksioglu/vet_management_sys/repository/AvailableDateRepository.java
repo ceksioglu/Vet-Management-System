2d18ad1f254e272a712e572acb46ecd828d75e85
ceksioglu <64319097+ceksioglu@users.noreply.github.com>
2024-07-17 10:29:30 +0300

    Create AvailableDateRepository.java

---
+++ backend/vet-management-sys/src/main/java/ceksioglu/vet_management_sys/repository/AvailableDateRepository.java
@@ -0,0 +1,7 @@
+package ceksioglu.vet_management_sys.repository;
+
+import ceksioglu.vet_management_sys.entity.AvailableDate;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AvailableDateRepository extends JpaRepository<AvailableDate,Long> {
+}
