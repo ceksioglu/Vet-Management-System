d04803bc3556aacedad3366daf55f4b19a978bed
ceksioglu <64319097+ceksioglu@users.noreply.github.com>
2024-07-17 10:28:40 +0300

    Create ResourceNotFoundException.java

---
+++ backend/vet-management-sys/src/main/java/ceksioglu/vet_management_sys/core/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package ceksioglu.vet_management_sys.core.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
