a321ac05c1c85ffa7d8e3eeab14fbfd2c3ca82fa
ceksioglu <64319097+ceksioglu@users.noreply.github.com>
2024-07-17 10:36:53 +0300

    Update AvailableDateRepository.java

--- backend/vet-management-sys/src/main/java/ceksioglu/vet_management_sys/repository/AvailableDateRepository.java
+++ backend/vet-management-sys/src/main/java/ceksioglu/vet_management_sys/repository/AvailableDateRepository.java
@@ -2,6 +2,30 @@ package ceksioglu.vet_management_sys.repository;
 
 import ceksioglu.vet_management_sys.entity.AvailableDate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface AvailableDateRepository extends JpaRepository<AvailableDate,Long> {
+import java.util.Date;
+import java.util.List;
+
+@Repository
+public interface AvailableDateRepository extends JpaRepository<AvailableDate, Long> {
+
+    /**
+     * Belirli bir doktor ve tarih için mevcut olan müsait günü kontrol eder.
+     *
+     * @param doctorId Doktorun ID'si
+     * @param availableDate Müsait günün tarihi
+     * @return Mevcut olan müsait gün
+     */
+    boolean existsByDoctorIdAndAvailableDate(Long doctorId, Date availableDate);
+
+    /**
+     * Belirli bir doktor ve tarih aralığı için mevcut olan müsait günleri döndürür.
+     *
+     * @param doctorId Doktorun ID'si
+     * @param startDate Başlangıç tarihi
+     * @param endDate Bitiş tarihi
+     * @return Belirli bir tarih aralığında ve doktora ait müsait günlerin listesi
+     */
+    List<AvailableDate> findByDoctorIdAndAvailableDateBetween(Long doctorId, Date startDate, Date endDate);
 }
